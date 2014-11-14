f68a91c5219132d21f81a99bdbb23843c57bcf2a
jamondouglas <terrildouglas@gmail.com>
2014-11-13 16:52:50 -0800

    server/config/config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 /*jslint node: true */
 module.exports = {
   port: process.env.PORT || 5000,
-  database: 'tipzip', 
-  username: 'postgres', 
-  password: 'myPassword',
-  host: process.env.DATABASE_URL ||'localhost',
+  database: process.env.DB_NAME || 'tipzip', 
+  username: process.env.DB_USER || 'postgres', 
+  password: process.env.DB_PASSWORD || 'myPassword',
+  host: process.env.DB_HOST ||'localhost',
   db_port: 5432,
-  dialect: 'postgres', //obviously you don't have to use PostgreSQL
+  dialect: 'postgres',
   native: true
 };
