7c202545795cd08c3e80c97d0cb07a453384bec5
jamondouglas <terrildouglas@gmail.com>
2014-11-13 16:20:30 -0800

    Coding style updates

--- server/db/seed/pedestrian/seedPedestrian.js
+++ server/db/seed/pedestrian/seedPedestrian.js
@@ -11,37 +11,36 @@ var Pedestrian = require('../../db').Pedestrian;
 */
 var insertPedestrianVolume = exports.insertPedestrianVolume = function(pedVolumeObj){
   console.log("Seeding Pedestrian data");
-
-  xlsParser({
+  var xlsParserInput = {
       input: __dirname + "/signalizedTrafficPedestrianVolumes.xls",
       output: null
       //sheet: "sheetname",  // specific sheetnam
-    }, function(err, result) {
-      if(err) {
-        console.error(err);
-      } else {
-        console.log("TRY AND INSERT DATA");
-        console.log(result.length);
-        for(var i=0; i<result.length;i++){
-          Pedestrian.build({
-            mainroute: result[i]['Main'],
-            sideroute: result[i]['Side 1 Route'],
-            latitude: result[i]['Latitude'],
-            longitude: result[i]['Longitude'],
-            pedestrianvol8hr: result[i]['8HrPedVol'],
-            pedestrianvol24hr: result[i]['24HrPedVol']
-          }).save()
-          .complete(function(err){
-            if(!!err){
-              console.log(" PEDESTRIAN This instance hasn't been saved:", err);
-            }else{
-              console.log('PEDESTRIAN Instance has been stored.');
-            }
-          });
-        }
+  };
+  xlsParser(xlsParserInput,function(err, result) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("TRY AND INSERT DATA");
+      console.log(result.length);
+      for(var i=0; i<result.length;i++){
+        Pedestrian.build({
+          mainroute: result[i]['Main'],
+          sideroute: result[i]['Side 1 Route'],
+          latitude: result[i]['Latitude'],
+          longitude: result[i]['Longitude'],
+          pedestrianvol8hr: result[i]['8HrPedVol'],
+          pedestrianvol24hr: result[i]['24HrPedVol']
+        }).save()
+        .complete(function(err){
+          if (!!err){
+            console.log(" PEDESTRIAN This instance hasn't been saved:", err);
+          }else{
+            console.log('PEDESTRIAN Instance has been stored.');
+          }
+        });
       }
-    });
-  
+    }
+  });
 };
 
 setTimeout(insertPedestrianVolume, 5000);
